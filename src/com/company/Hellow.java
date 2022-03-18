93e4f5b9c4e01538d228a36e166e2cc9f12e17a5
AndrewPleskanko <pleskankoandrew@gmail.com>
2022-03-18 15:17:26 +0200

    add hello file

---
+++ src/com/company/Hellow.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Hellow {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+
+
+
+    }
+}

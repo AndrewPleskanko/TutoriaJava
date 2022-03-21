f6b5a3eed9f3657b93245be8a239ce9ab74bcc4e
AndrewPleskanko <pleskankoandrew@gmail.com>
2022-03-21 22:47:18 +0200

    new project

--- src/com/company/Warriors.java
+++ src/com/company/Warriors.java
@@ -3,15 +3,16 @@ package com.company;
 import java.util.Scanner;
 
 public class Warriors {
-     static int id;
-     String name;
-     int hp;
-     int attackForce;
-     float miss;
-     float armor;
-     int sumSoldier;
+    int id;
+    String name;
+    int hp;
+    int attackForce;
+    float miss;
+    float armor;
+    int sumSoldier;
 
-    public Warriors( String name, int hp, int attackForce, float miss, float armor, int sumSoldier) {
+    public Warriors(int id, String name, int hp, int attackForce, float miss, float armor, int sumSoldier) {
+        this.id = id;
         this.name = name;
         this.hp = hp;
         this.attackForce = attackForce;
@@ -19,6 +20,7 @@ public class Warriors {
         this.armor = armor;
         this.sumSoldier = sumSoldier;
     }
+
     public Warriors(int hp, int attackForce, float miss, float armor, int sumSoldier) {
         this.hp = hp;
         this.attackForce = attackForce;
@@ -26,46 +28,79 @@ public class Warriors {
         this.armor = armor;
         this.sumSoldier = sumSoldier;
     }
-    void agresive(int attackForce, float miss ){
+
+    public static void main(String[] args) {
+
+        Warriors warrior1 = new Warriors(1, "bowman", 100, 20, 3f, 1.4f, 1);
+        Warriors warrior2 = new Warriors(2, "knight", 115, 15, 1.2f, 1, 1);
+        Warriors warrior3 = new Warriors(3, "magician", 90, 17, 1f, 1, 1);
+
+        System.out.println("write warrior for combat and tactic - player#1");
+        System.out.println("1-knight; 2-bowman; 3-magician");
+        Scanner scanner = new Scanner(System.in);
+        int hero = scanner.nextInt();
+        System.out.println("1-agresive; 2-defaultAtac; 3-passive");
+        int attackMode = scanner.nextInt();
+        switch (hero) {
+            case 1:
+                switch (attackMode) {
+                    case 1:
+                        agresive(warrior1.attackForce, warrior1.miss);
+                        break;
+                    case 2:
+                        defaultAtac(warrior1.attackForce, warrior1.miss);
+                        break;
+                    case 3:
+                        passive(warrior1.attackForce, warrior1.miss);
+                        break;
+                    default:
+                        System.out.println("incorect value(tactic)");
+                }
+            case 2:
+                switch (attackMode) {
+                    case 1 -> agresive(warrior2.attackForce, warrior2.miss);
+                    case 2 -> defaultAtac(warrior2.attackForce, warrior2.miss);
+                    case 3 -> passive(warrior2.attackForce, warrior2.miss);
+                    default -> System.out.println("incorect value(tactic)");
+                }
+            case 3:
+                switch (attackMode) {
+                    case 1:
+                        agresive(warrior3.attackForce, warrior3.miss);
+                        break;
+                    case 2:
+                        defaultAtac(warrior3.attackForce, warrior3.miss);
+                        break;
+                    case 3:
+                        passive(warrior3.attackForce, warrior3.miss);
+                        break;
+                    default:
+                        System.out.println("incorect value(tactic)");
+                }
+            default:
+                System.out.println("incorect value(warrior)");
+
+        }
+    }
+
+    static void agresive(int attackForce, float miss) {
+        Warriors warriosHelp1 = new Warriors(1, 1, 1.1f, 1, 1);
         attackForce *= 3;
         miss *= 1.5f;
+
     }
-    void defaultAtac(int attackForce, float miss){
+
+    static void defaultAtac(int attackForce, float miss) {
         attackForce *= 1;
         miss *= 1;
     }
-    void passive(int attackForce, float miss){
+
+    static void passive(int attackForce, float miss) {
         attackForce *= 2;
         miss = 0;
     }
-    public static void main(String[] args) {
-        Warriors warrior1 = new Warriors("bowman",100,20,3,1.4f,1);
-        Warriors warrior2 = new Warriors("knight",115,15, 1.3f,1,1);
-        Warriors warrior3 = new Warriors("magician",90,26, 1.5f,1,1);
-
-        Warriors warriosHelp2 = new Warriors(1,1,1,1,1);
-            System.out.println("write warrior for combat and tactic - player#1");
-            System.out.println("1-knight; 2-bowman; 3-magician");
-        Scanner scanner = new Scanner(System.in);
-        String a = scanner.nextLine();
-            System.out.println("1-agresive; 2-defaultAtac; 3-passive");
-        String b = scanner.nextLine();
-
-
-       switch(a.equals(warrior1.name) || a.equals(warrior2.name)  ){
-           case 1: Warriors warriosHelp1 = new Warriors(warrior1.hp, warrior1.attackForce, warrior1.miss, warrior1.armor, 1);
-break;
-
-               case2:warriosHelp1 = new Warriors(1,1,1,1,1);
-               case3:warriosHelp1 = new Warriors(1,1,1,1,1);
-           default:
-               System.out.println("incorrent warrior");
-               break;
-       }
-        System.out.println("select methods for combat player#1");
-        System.out.println("1-knight; 2-bowman; 3-magician");
 
 
 }
-    }
+
 
